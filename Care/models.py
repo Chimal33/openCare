febd83e4f9a05a61c74fc99026d3eefe8e8b88b8
chimal33 <chiselebwemargaret66@gmail.com>
2018-02-02 12:33:56 +0200

    second commit

---
+++ Care/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_Name= models.CharField(max_length=200,blank=True)
+    age= models.IntegerField(blank=True)
+    sex= models.CharField(max_length=200,blank=True)
+    date_of_Birth= models.IntegerField(blank=True)
+    home_Address= models.CharField(max_length=200,blank=True)
+    phone_Number= models.IntegerField(blank=True)
+    next_of_kin= models.CharField(max_length=200,blank=True)
+    email_Address=models.EmailField(max_length=200,blank=True)
+
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+
+    def __str__(self):
+        return self.full_Name
